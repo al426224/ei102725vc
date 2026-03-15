bc749da46eaabe25977017a713ad787930f57a52
PauCalaT <pauhenhao@gmail.com>
2026-03-15 21:21:09 +0100

    Prueba por fallo github

--- src/main/java/es/uji/ei1027/SgOVI/model/UsuarioOVI.java
+++ src/main/java/es/uji/ei1027/SgOVI/model/UsuarioOVI.java
@@ -35,7 +35,7 @@ public class UsuarioOVI {
         this.proyectoVida = proyectoVida;
         this.estado = estado;
     }
-    //mirar si añadir o no contraseña y si hacerlo en una superclase.
+    //mirar si añadir o no contraseña y si hacerlo en una superclase
 
     public String getIdUsuario() {
         return idUsuario;
