a15afd84ded9f9f9d08125a8bd79a948fabdf413
PauCalaT <tu-email@email.com>
2026-03-15 19:36:30 +0100

    feat: crear DAO Seleccion

---
+++ src/main/java/es/uji/ei1027/SgOVI/rowMapper/SeleccionRowMapper.java
@@ -0,0 +1,21 @@
+package es.uji.ei1027.SgOVI.rowMapper;
+
+import es.uji.ei1027.SgOVI.model.Seleccion;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SeleccionRowMapper implements RowMapper<Seleccion> {
+
+    @Override
+    public Seleccion mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Seleccion seleccion = new Seleccion();
+        seleccion.setIdSeleccion(rs.getInt("id_seleccion"));
+        seleccion.setIdSolicitud(rs.getString("id_solicitud"));
+        seleccion.setIdAsistente(rs.getString("id_asistente"));
+        seleccion.setEstadoSeleccion(rs.getString("estado_seleccion"));
+        seleccion.setPuntuacionMatch(rs.getObject("puntuacion_match", Integer.class));
+        return seleccion;
+    }
+}
