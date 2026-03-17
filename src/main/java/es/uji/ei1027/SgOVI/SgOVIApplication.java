ef6ae0deb3a462064ba2b4b45de35907738b8fa8
PauCalaT <pauhenhao@gmail.com>
2026-03-17 01:00:45 +0100

    tets

---
+++ src/main/java/es/uji/ei1027/SgOVI/SgOVIApplication.java
@@ -0,0 +1,12 @@
+package es.uji.ei1027.SgOVI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgOVIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgOVIApplication.class, args);
+    }
+}
