4b103893171147f3fd58d69fd36478960ed8aed5
PauCalaT <pauhenhao@gmail.com>
2026-04-12 03:01:54 +0200

    feat: creación de html

--- src/main/java/es/uji/ei1027/SgOVI/controller/SignupController.java
+++ src/main/java/es/uji/ei1027/SgOVI/controller/SignupController.java
@@ -4,10 +4,12 @@ import es.uji.ei1027.SgOVI.dao.UsuarioOVIDao;
 import es.uji.ei1027.SgOVI.model.UsuarioOVI;
 import es.uji.ei1027.SgOVI.validator.UsuarioOVISignupValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -21,6 +23,11 @@ public class SignupController {
     @Autowired
     public SignupController(UsuarioOVIDao usuarioOVIDao) {
         this.usuarioOVIDao = usuarioOVIDao;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 
     @GetMapping
