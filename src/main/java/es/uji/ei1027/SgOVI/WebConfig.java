bb800df8dbe542f6b7ac4d9fd52b2cdc52493fef
PauCalaT <pauhenhao@gmail.com>
2026-06-05 20:17:23 +0200

    fix: mover a carpeta apropiada

--- src/main/java/es/uji/ei1027/SgOVI/WebConfig.java
+++ src/main/java/es/uji/ei1027/SgOVI/WebConfig.java
@@ -2,17 +2,12 @@ package es.uji.ei1027.SgOVI;
 
 import es.uji.ei1027.SgOVI.model.Rol;
 import es.uji.ei1027.SgOVI.model.RolInterceptor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${app.upload.base-path}")
-    private String uploadPath;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -32,9 +27,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/peticionAP/**");
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadPath + "/");
-    }
 }
